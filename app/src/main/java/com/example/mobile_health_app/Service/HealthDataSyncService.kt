eb2e535ebf2e6638496fdbdda4bff2942bb4636f
Huy Võ Quốc <vqh17062001@gmail.com>
2025-06-25 08:41:16 +0700

    feat: Add HealthDataSyncService and permissions for foreground service data synchronization

---
+++ app/src/main/java/com/example/mobile_health_app/Service/HealthDataSyncService.kt
@@ -0,0 +1,16 @@
+package com.example.mobile_health_app.Service
+
+import android.content.Context
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import android.util.Log
+
+class HealthDataSyncService: Service() {
+
+
+
+    override fun onBind(intent: android.content.Intent?) = null
+
+    // Implement sync logic here
+}
